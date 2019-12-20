a2b00214d4fa01b692040dc344195ed86d3cdff1
Hacker-kingdom-TH673 <58379849+Hacker-kingdom-TH673@users.noreply.github.com>
2019-12-20 21:48:17 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+#Creator is Hacker kingdom TH
+#Start Script
+
+:Installation Steps
+
+$pkg upgarde
+
+$pkg updare
+
+$pkg install git
+
+$pkg install python
+
+$pkg install python2
+
+$pkg install figlet
+
+$git clone https://github.com/Hacker-kingdom-TH673/Hotporn
+
+:Steps Of Use
+
+$cd Hotporn
+
+$python2 codeporn
+
+The End
---
+++ codeporn.py
@@ -0,0 +1,26 @@
+#hello im Creator Script Pornhub..!!
+#Start run script hacking
+
+import os,sys
+
+os.system('pkg upgarde')
+print ('')
+raw_input('Please Entered To Continued...')
+os.system('clear')
+print
+print ('Code Pornhub')
+print ('exit To Enter')
+print ("Look at menu")
+os.system("figlet Copy code paste at google")
+print ('!!...Link code at menu...!!')
+print ('\033[1m\033[32m  [1] creampie               ')
+print ('\033[1m\033[32m  [2] J A V                  ')
+print ('\033[1m\033[32m  [3] Jav uncensored  ')
+print ('\033[1m\033[32m  [4] Orgy gangbang    ')
+print ('\033[0m')
+print
+print ('\033[1m\033[32m Programin By Hacker kingdom TH')
+
+
+raw_input('Boy18+>')
+print "\033[1m\033[32m Python Programing By Hacker kingdom TH"
